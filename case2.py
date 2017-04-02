8ac4868bd536cae4714992f1c7c55523f50cc4da
Isha Mehrotra <isha.mehrotra@hotmail.com>
2017-04-02 17:26:03 -0500

    repushing

--- case2.py
+++ case2.py
@@ -2,7 +2,8 @@
 
 from algo_client import *
 from pprint import pprint
-from vollib import black
+from vollib import black_scholes as b_s
+import greeks_new.py as g
 
 def ourOrderBook(algo_client):
 
@@ -33,7 +34,23 @@ def priceAndGreekData(algo_client):
 
 	if market_depth1["bids"]
 
+#TODO
+#1. Pick an option/price (python API)
 
+#2. Back out implied volatility (vollib)
+#vollib.black_scholes.implied_volatility.implied_volatility(price, S, K, t, r, flag)
+#it's in greeks calculator!
+def back_out(CallPutFlag, price, underlying, strike, time_to_expiry, interest):
+	return g.implied_vol(CallPutFlag, price, underlying, strike, time_to_expiry, interest)
+#how do I get the inputs, need to access market and trade data...wait it does it for me so just take it from what greeks outputs...
+#ok so now how do I use given greeks calculator
 
+#3. Take IV and calculate theoretical price of other options (vollib)
+	return b_s.black_scholes(flag, S, K, t, r, sigma)
+#4. Find differences between actual option price and theoretical (math/coding)
+
+#5. Make market (greatest->least) (TBD)
+
+#6. Hedge 
 
 
