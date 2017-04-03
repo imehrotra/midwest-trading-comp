ba2122e40e0561d027e480814e230da2c2a8b8f5
James Kon <konjames@Jamess-MacBook-Pro-2.local>
2017-04-03 00:01:48 -0500

    started working on case 1

--- case1.py
+++ case1.py
@@ -2,8 +2,7 @@
 #2.calculate the derived yield by measuring needed interest rate between 1-yr treasury future and 5-yr treasury future
 #3.take a position on the yield curve -if the derived yield is greater 
 #than the interest rate, then steepening yeild curve (buy near contract and sell far contract )
-#4. Hedge by making sure the dollar value of 1% change in the yield of the short term future equals that of the long term
-#may try different hedging strategies
+#4. Hedge by making sure the dollar value of 1% change in the yield of the short term future equals that of the long term (DV01)
 
 
 from algo_client import *
